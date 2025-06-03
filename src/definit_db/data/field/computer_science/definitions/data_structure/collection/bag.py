60a4fd2386504096af89b48e349a7c04dfe337f3
K4liber <janbielecki94@gmail.com>
2025-06-03 21:36:50 +0200

    fix imports & bump version

--- src/definit_db/data/field/computer_science/definitions/data_structure/collection/bag.py
+++ src/definit_db/data/field/computer_science/definitions/data_structure/collection/bag.py
@@ -1,4 +1,4 @@
-from definit_db.data.field.computer_science.definitions.data_structure.collection import COLLECTION
+from definit_db.data.field.computer_science.definitions.data_structure.collection.collection import COLLECTION
 from definit_db.data.field.mathematics.definitions.foundamental.multiset import MULTISET
 from definit_db.definition.definition import Definition
 from definit_db.definition.definition_key import DefinitionKey
@@ -7,7 +7,12 @@ from definit_db.definition.field import Field
 
 class _Bag(Definition):
     def _get_content(self) -> str:
-        return f"A bag is a {COLLECTION.key.get_reference(phrase='collection')} that allows for the storage of multiple items, where the same item can be stored multiple times. It is similar to a {MULTISET.key.get_reference(phrase='multiset')}, but does not require any specific ordering of the items. Removing items is not supported. A bag allows to iterate through the collected items."
+        return (
+            f"A bag is a {COLLECTION.key.get_reference(phrase='collection')} that allows for the storage of "
+            "multiple items, where the same item can be stored multiple times. It is similar to a "
+            f"{MULTISET.key.get_reference(phrase='multiset')}, but does not require any specific ordering of "
+            "the items. Removing items is not supported. A bag allows to iterate through the collected items."
+        )
 
 
 BAG = _Bag(
--- src/definit_db/data/field/computer_science/definitions/data_structure/collection/stack.py
+++ src/definit_db/data/field/computer_science/definitions/data_structure/collection/stack.py
@@ -1,4 +1,4 @@
-from definit_db.data.field.computer_science.definitions.data_structure.collection import COLLECTION
+from definit_db.data.field.computer_science.definitions.data_structure.collection.collection import COLLECTION
 from definit_db.data.field.computer_science.definitions.data_structure.collection.queue import QUEUE
 from definit_db.data.field.computer_science.definitions.foundamental.data_structure import DATA_STRUCTURE
 from definit_db.data.field.computer_science.definitions.foundamental.operation import OPERATION
@@ -11,11 +11,16 @@ from definit_db.definition.field import Field
 class _Stack(Definition):
     def _get_content(self) -> str:
         return (
-            f"Stack is a {DATA_STRUCTURE.key.get_reference(phrase='data structure')} providing last-in-first-out semantics; also called a LIFO {QUEUE.key.get_reference(phrase='queue')}.\n\n"
-            f"Serves as a {COLLECTION.key.get_reference(phrase='collection')} of elements with two main {OPERATION.key.get_reference(phrase='operations')}:\n\n"
+            f"Stack is a {DATA_STRUCTURE.key.get_reference(phrase='data structure')} providing last-in-first-out "
+            f"semantics; also called a LIFO {QUEUE.key.get_reference(phrase='queue')}.\n\n"
+            f"Serves as a {COLLECTION.key.get_reference(phrase='collection')} of elements with two main "
+            f"{OPERATION.key.get_reference(phrase='operations')}:\n\n"
             "- Push, which adds an element to the collection, and\n"
             "- Pop, which removes the most recently added element.\n\n"
-            f"Additionally, a peek {OPERATION.key.get_reference(phrase='operation')} can, without modifying the stack, return the value of the last element added. The name stack is an analogy to a {SET.key.get_reference(phrase='set')} of physical items stacked one atop another, such as a stack of plates."
+            f"Additionally, a peek {OPERATION.key.get_reference(phrase='operation')} can, without modifying the stack, "
+            "return the value of the last element added. The name stack is an analogy to a "
+            f"{SET.key.get_reference(phrase='set')} of physical items stacked one atop another, "
+            "such as a stack of plates."
         )
 
 

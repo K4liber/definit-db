59897a1f2ba0b4b6f5667748795ddc799f9dc3c4
K4liber <janbielecki94@gmail.com>
2025-11-25 14:07:37 +0100

    test

--- src/definit_db/data/field/mathematics/definitions/fundamental/object.py
+++ src/definit_db/data/field/mathematics/definitions/fundamental/object.py
@@ -5,10 +5,10 @@ from definit_db.definition.field import Field
 
 class _Object(Definition):
     def _get_content(self) -> str:
-        return (
-            "An object is an abstract concept arising in mathematics. "
-            "Typically, a mathematical object can be a value that can be assigned to a symbol."
-        )
+        return """
+An object is an abstract concept arising in mathematics. 
+Typically, a mathematical object can be a value that can be assigned to a symbol.
+"""
 
 
 OBJECT = _Object(
